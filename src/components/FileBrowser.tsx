1539e6db70363b95aca920441900be2778919a89
Éric ALBER <eric.alber@gmail.com>
2018-12-04 22:41:46 +0100

    client: Add icons on FileBrowser

--- src/components/FileBrowser.tsx
+++ src/components/FileBrowser.tsx
@@ -1,16 +1,27 @@
 import * as React from "react";
 import { createStyles, withStyles, WithStyles } from '@material-ui/core/styles';
 import FolderIcon from '@material-ui/icons/Folder';
+import FolderLockIcon from 'mdi-material-ui/FolderLock';
+import FileLockIcon from 'mdi-material-ui/FileLock';
+import { grey } from '@material-ui/core/colors';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
+import Typography from "@material-ui/core/Typography";
 import Checkbox from '@material-ui/core/Checkbox';
 import { RouteComponentProps } from 'react-router-dom';
 import PaperList from './PaperList';
 import * as connection from '../connection';
 import { Ls } from '../apitypes';
+import { getFileIcon, getPrettySize, getPrettySizeUnit } from '../tools';
 
 const styles = createStyles({
+  line: {
+    paddingLeft: 0,
+  },
+  checkbox: {
+    color: grey[300]
+  }
 });
 
 type State = {
@@ -27,7 +38,6 @@ class FileBrowser extends React.Component<Props & WithStyles<typeof styles>, Sta
 
   async componentWillMount() {
     const path = this.props.location.pathname.substr(this.props.match.path.length);
-    console.log(this.props);
     let data = await connection.get('/api/ls' + path) as Ls;
     this.setState({data: data});
   }
@@ -37,17 +47,35 @@ class FileBrowser extends React.Component<Props & WithStyles<typeof styles>, Sta
       return null;
     }
     const data = this.state.data;
+    const classes = this.props.classes;
+    let icontype = getFileIcon("blabla");
+    if (icontype !== null) {
+      React.createElement(icontype);
+    }
     return (
       <PaperList>
-        {data.content.map((item, index) =>
-          <ListItem button key={index}>
-            <Checkbox disableRipple tabIndex={-1}/>
-            <ListItemIcon><FolderIcon/></ListItemIcon>
+        {data.content.map((item, index) => {
+          let icon = null;
+          if (item.isDir) {
+            icon = item.isReadable ? FolderIcon : FolderLockIcon;
+          } else {
+            icon = item.isReadable ? getFileIcon(item.name) : FileLockIcon;
+          }
+          return (
+          <ListItem key={index} button classes={{root: classes.line}} onClick={(e) => this.onClick(e, index)}>
+            <Checkbox disableRipple tabIndex={-1} classes={{root: classes.checkbox}}/>
+            <ListItemIcon>{React.createElement(icon)}</ListItemIcon>
             <ListItemText>{item.name}</ListItemText>
+            <Typography variant='caption' color='textSecondary'>{getPrettySize(item.size)} {getPrettySizeUnit(item.size)}</Typography>
           </ListItem>
-        )}
+        )})}
       </PaperList>
     )
+  }
+  onClick(event: React.SyntheticEvent, index: number) {
+    if (this.state.data) {
+      console.log(this.state.data.content[index]);
+    }
   }
 }
 
