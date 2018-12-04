48710260cd7185ff72127aae900d3b8fa59cd1b2
Éric ALBER <eric.alber@gmail.com>
2018-12-04 22:37:57 +0100

    client: Add /api/ls types

--- src/apitypes.ts
+++ src/apitypes.ts
@@ -1,13 +1,13 @@
 // Types for /api/info
 
-export type StoragePathInfo = {
+export type InfoStoragePath = {
     name: string,
     path: string,
     availableBytes: number,
     totalBytes: number
 }
 
-export type ShortcutDirectoryInfo = {
+export type InfoShortcutDirectory = {
     exists: boolean,
     path: string
 }
@@ -19,20 +19,38 @@ export type Info = {
     premium: boolean,
     sdk: number,
     directories: {
-        alarms: ShortcutDirectoryInfo,
-        camera: ShortcutDirectoryInfo,
-        documents: ShortcutDirectoryInfo,
-        downloads: ShortcutDirectoryInfo,
-        movies: ShortcutDirectoryInfo,
-        music: ShortcutDirectoryInfo,
-        notifications: ShortcutDirectoryInfo,
-        pictures: ShortcutDirectoryInfo,
-        podcasts: ShortcutDirectoryInfo,
-        ringtones: ShortcutDirectoryInfo,
-        [index:string]: ShortcutDirectoryInfo,
+        alarms: InfoShortcutDirectory,
+        camera: InfoShortcutDirectory,
+        documents: InfoShortcutDirectory,
+        downloads: InfoShortcutDirectory,
+        movies: InfoShortcutDirectory,
+        music: InfoShortcutDirectory,
+        notifications: InfoShortcutDirectory,
+        pictures: InfoShortcutDirectory,
+        podcasts: InfoShortcutDirectory,
+        ringtones: InfoShortcutDirectory,
+        [index:string]: InfoShortcutDirectory,
     },
     storagePaths: {
-        internal: StoragePathInfo,
-        externals: [ StoragePathInfo ],
+        internal: InfoStoragePath,
+        externals: [ InfoStoragePath ],
     },
+}
+
+export type Ls = {
+  content: [{
+    isDir: boolean,
+    isReadable: boolean,
+    isWritable: boolean,
+    mTime: number,
+    mime: string,
+    name: string,
+    size: number,
+  }],
+  isDir: boolean,
+  isReadable: boolean,
+  isWritable: boolean,
+  mTime: number,
+  name: string,
+  size: number,
 }
